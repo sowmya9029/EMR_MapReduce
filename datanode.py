326bf06ce7cbeed38b9a3087c4f4d6f3c39e74f4
alice Zhang <alicecheung505@gmail.com>
2018-02-18 15:57:27 -0800

    create datanode.py

---
+++ datanode.py
@@ -0,0 +1,22 @@
+import os
+import rpyc
+import uuid
+from rpyc.utils.server import ThreadedServer
+
+class DataNode:
+	# DataNode stores the actual data
+
+	def get_file():
+
+	def put_file():
+
+	def forward_file():
+
+	def delete_file():
+
+	# def heartbeat():
+
+	# def get_report():
+
+
+
